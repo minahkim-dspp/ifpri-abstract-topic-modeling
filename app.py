fcdc34e70ed872eebf133a78a45e3eea65f967a6
MinAh Kim <112434777+minahkim-dspp@users.noreply.github.com>
2023-11-22 19:19:28 -0500

    initial app design

---
+++ app.py
@@ -0,0 +1,129 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from preprocessing import lda_process, Lemmatization_Tokenizer
+
+import random
+
+from annotated_text import annotated_text, parameters
+
+import wordcloud
+from matplotlib import pyplot as plt
+
+### 1. Data to Visualize 
+
+# Data, LDA object and the original Document Term Matrix
+df, lda, dtm = lda_process(number_topic = 6)
+
+# Topics and their words 
+component_array = lda.components_/lda.components_.sum(axis =1)[:, np.newaxis]
+component_df = pd.DataFrame(component_array, columns = lda.feature_names_in_)
+
+# Documents and their topics
+document_topic_df = pd.DataFrame(lda.transform(dtm))
+
+# Randomly choose the text that we will analyze
+document_num = random.choice(range(0, df.shape[0]))
+abstract = df.Abstract.iloc[document_num]
+
+### 2. Building the backbone of the website
+
+# Title
+st.title("Topic Modeling with the Abstract of the IFPRI Policy Brief")
+
+# Color Scheme
+tol_light_color =["#77AADD", "#EE8866", "#EEDD88", "#FFAABB", "#99DDFF", "#44BB99", "#BBCC33", "#AAAA00", "#DDDDDD"]
+
+# First Header
+st.header("Abstract")
+
+## Abstract
+# Importing the lemmatization and tokenization process that the text went through
+lemmatization_and_tokenizing = Lemmatization_Tokenizer()
+# Spliting the abstract into a token level. This abstract should be kept as it is.
+abstract_in_word = lemmatization_and_tokenizing.tokenizer(abstract)
+# Create a "separate" list that has the tokenized/lemmatized/non-stopword words. 
+words_that_count = [lemmatization_and_tokenizing(word) for word in abstract_in_word]
+
+# Save the topic that the word represents by order of the word
+topic_by_order_of_word = []
+
+for word_in_dict in words_that_count:
+    try:
+        # If the word does not exist (i.e. excluded because it is a number or stopword), the topic should be "NoTopic"
+        if word_in_dict == []:
+            topic_by_order_of_word.append("NoTopic")
+        elif word_in_dict != []:
+            # extract the string form of the token
+            tokenized_word = word_in_dict[0]
+            # add the topic number of the topic that the word has the highest contribution
+            topic_by_order_of_word.append(component_df[tokenized_word].sort_values(ascending = False).index[0])
+    except:
+        # Any other unidentifiable case will also fall under "NoTopic"
+        topic_by_order_of_word.append("NoTopic")
+
+# Create the list to use for annotated text using the two lists above
+abstract_text_with_annotation = []
+
+for i in range(0, len(topic_by_order_of_word)):
+    # We keep only the original word if it has no topic.
+    if topic_by_order_of_word[i] == "NoTopic":
+        abstract_text_with_annotation.append(abstract_in_word[i]+" ")
+    else:
+    # If it has a topic assigned, we create a tuple with the (word, topic, color of the topic)
+        topic_for_the_word = int(topic_by_order_of_word[i])
+        abstract_text_with_annotation.append((abstract_in_word[i], str(topic_for_the_word), tol_light_color[topic_for_the_word]))
+
+annotated_text(abstract_text_with_annotation)
+
+
+# Second Header
+st.header("What are the topics that we identified?")
+
+## Word Cloud
+fig = plt.figure()
+
+for i in range(0, lda.n_components):
+    # Add a subplot
+    ax = fig.add_subplot(3, 2, i+1)
+    
+    # The first 10 words that represent the data
+    representation = component_df.iloc[i].sort_values(ascending= False).head(20)
+
+    # Create a wordcloud
+    wc = wordcloud.WordCloud(background_color = "white", 
+                                color_func = lambda *args, **kwargs: tol_light_color[i]).generate_from_frequencies(representation)
+
+    ax.imshow(wc)
+    ax.axis('off')
+
+st.pyplot(fig)
+
+# Third Header
+st.header("The Distribution of Topics of this Abstract")
+
+# Start a plot
+fig_1, ax_1 = plt.subplots()
+
+# Extract the information about the document from document_topic_df
+this_document_topic_df = document_topic_df[document_topic_df.index == document_num]
+
+# Create a bar graph
+ax_1.bar(this_document_topic_df.columns, 
+            this_document_topic_df.iloc[0],
+            color= tol_light_color[0:lda.n_components])
+
+# Set y label
+ax_1.set_ylabel("The Distribution of the Topic")
+# Set the limit of y axis so that it won't change across documents
+ax_1.set_ylim([0,1])
+# Set x label
+ax_1.set_xlabel("Topic")
+
+# Show the plot
+st.pyplot(fig_1)   
+
+
+
+
---
+++ preprocessing.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import re
+from nltk.stem.wordnet import WordNetLemmatizer
+from sklearn.feature_extraction import text 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+class DataProcessing:
+    '''
+    Import the CSV and conduct basic filtering and selection.
+    This class exists to keep various version of the dataset in a single object
+    '''
+    def __init__(self, csv) -> None:
+        self.raw_df = pd.read_csv(csv)
+        self.df_of_interest = self.raw_df[['Title', 'Year', 'Author', 'Abstract', 'IFPRI Descriptors', 'Subject - author supplied keywords',
+       'Subject - country location', 'Subject - keywords']]
+        self.with_abstract = self.df_of_interest[self.df_of_interest.Abstract.isnull() == False]
+        self.no_abstract = self.df_of_interest[self.df_of_interest.Abstract.isnull()]
+    
+class Lemmatization_Tokenizer(object):
+    '''
+    Integrate Lemmatizer from the NLTK package to the Scikit-Learn CountVectorizer/TF-IDF Vectorizer
+
+    Return: List with lemmatize tokens
+    '''
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+        self.countvect = CountVectorizer(stop_words='english')
+        self.tokenizer = self.countvect.build_tokenizer()
+        self.stop_word = self.countvect.get_stop_words()
+
+        # Add stop words
+        #my_additional_stop_words = ("the", "a")
+        #self.stop_word = text.ENGLISH_STOP_WORDS.union(my_additional_stop_words)
+
+    def __call__(self, abstract):
+        abstract_in_token = [word for word in self.tokenizer(abstract)]
+        abstract_no_stopword = [word for word in abstract_in_token if word.casefold() not in self.stop_word]
+        abstract_no_number = [word for word in abstract_no_stopword if bool(re.search(r"[0-9]+", word)) == False]
+
+        return [self.wnl.lemmatize(token) for token in abstract_no_number]
+
+def display_topics(model, feature_names, no_top_words):
+    for topic_index,topic in enumerate(model.components_):
+        feature_topic = []
+
+        # sort it, and jump it every -1?
+        topic_word_indexes = topic.argsort()[::-1]
+        top_indexes = topic_word_indexes[:no_top_words]
+
+        feature_topic.append([feature_names[i] for i in top_indexes])
+        
+        return feature_topic
+         
+def lda_process(number_topic = 6):
+
+    ## 1. Read in the dataset & subset it
+    df_base = DataProcessing("ifpri_brief_df.csv")
+
+    ## 2. Lemmitization & Vectorization
+    # Initiate the TF-IDF with lemmitization
+    # The minimum term frequency was set to 15
+    text_vector = TfidfVectorizer(stop_words= "english", min_df = 15, tokenizer=Lemmatization_Tokenizer(), token_pattern = None)
+
+    ## 3. TF-IDF Document Term Matrix
+    # Array
+    text_vector_result = text_vector.fit_transform(df_base.with_abstract.Abstract).toarray()
+    feature_names = text_vector.get_feature_names_out()
+
+    # Dataframe
+    index_word = {index: text for text, index in text_vector.vocabulary_.items()}
+    index_word = [index_word[num] for num in range(0, len(index_word))]
+    text_vector_result = pd.DataFrame(text_vector_result, columns = index_word)
+
+    ## 4. Latent Dirichlet Allocation (LDA)
+    lda = LatentDirichletAllocation(n_components = number_topic, random_state = 0)
+    lda.fit(text_vector_result) 
+    display_topics(lda, feature_names, 15)
+
+
+    return df_base.with_abstract, lda, text_vector_result
+
+
+
+
+
+
